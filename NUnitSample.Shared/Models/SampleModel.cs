c596db7d4550fbc6895860cb22285b24e0eb1b12
Satoru Fujimori <satoru.fujimori@gmail.com>
2017-07-31 13:29:34 +0900

    Add NUnit and Shared project

---
+++ NUnitSample.Shared/Models/SampleModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitSample.Shared.Models
+{
+    public class SampleModel
+    {
+        public static int Calc(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ NUnitSample.Test/SampleModelTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnitSample.Shared.Models;
+
+namespace NUnitSample.Test
+{
+    [TestFixture]
+    public class SampleModelTest
+    {
+        [Test]
+        public void CalcTest()
+        {
+            Assert.AreEqual(3, SampleModel.Calc(1,2));
+        }
+    }
+}
